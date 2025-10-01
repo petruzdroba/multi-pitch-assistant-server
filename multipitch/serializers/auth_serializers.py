fee8eae1987bc888816d07a21e5dc4928a626432
Petru Zdroba <petru.zdroba@gmail.com>
2025-10-01 21:45:03 +0300

    feat: add SignupSerializer for user registration

---
+++ multipitch/serializers/auth_serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        style={'input_type': 'password'}
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        # Create the user with hashed password
+        user = User(
+            username=validated_data['username'],
+            email=validated_data.get('email')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
